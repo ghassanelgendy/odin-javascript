f85701f3c8b18f056eb19cfb350c62c0e83670d8
Ghassan <ghassanelgendyy@gmail.com>
2023-07-09 15:45:56 +0300

    Trying 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
 const reverseString = function (string) {
-	return string.split("").reverse().join("");
+	console.log(string.split("").reverse().join(""));
 };
 console.log(reverseString("ake"));
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, toRemove) {
+	newArray = [];
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] != toRemove) {
+			newArray.push(array[i]);
+		}
+	}
+	return newArray;
 };
-
 // Do not edit below this line
+console.log(removeFromArray([1, 2, 3, 4], 3));
 module.exports = removeFromArray;
