6b86b45032c68cb3dd216e468dc19ee2407be779
Ghassan <ghassanelgendyy@gmail.com>
2023-07-09 14:12:42 +0300

    Solve 01~03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, number = 1) {
+	if (number < 0) {
+		return "ERROR";
+	} else {
+		let newString = "";
+		for (let i = 0; i < number; i++) {
+			newString = newString.concat(string);
+		}
+		return newString;
+	}
 };
-
 // Do not edit below this line
 module.exports = repeatString;
