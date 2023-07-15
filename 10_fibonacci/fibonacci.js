002206b2310ca9275e1c454b934e50afcb9e931e
Ghassan <ghassanelgendyy@gmail.com>
2023-07-15 21:01:14 +0300

    Solved from 10 .-> 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,10 @@
-const fibonacci = function () {};
-
+const fibonacci = function (num) {
+	if (num < 2) {
+		return num;
+	} else {
+		return fibonacci(num - 1) + fibonacci(num - 2);
+	}
+};
+console.log(fibonacci(6));
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+	return arr.reduce((before, after) => {
+		const a = before.yearOfDeath - before.yearOfBirth;
+		const b = after.yearOfDeath - after.yearOfBirth;
+		return a > b ? before.name : after.name;
+	});
 };
+const people = [
+	{
+		name: "Carly",
+		yearOfBirth: 1942,
+		yearOfDeath: 1970,
+	},
+	{
+		name: "Ray",
+		yearOfBirth: 1962,
+		yearOfDeath: 2011,
+	},
+	{
+		name: "Jane",
+		yearOfBirth: 1912,
+		yearOfDeath: 1941,
+	},
+];
 
+console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
